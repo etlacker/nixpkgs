63463033063ca81db27ff35a236bd591555a282f
Jacob Ranson <code@jacobranson.dev>
2023-02-19 16:21:37 -0500

    Added back home folder configuration

--- home.nix
+++ home.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "jacobranson";
+  home.homeDirectory = "/Users/jacobranson";
+
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
