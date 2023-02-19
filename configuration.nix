e22157d7aa079a869df45eeeab8fa045051152cb
Jacob Ranson <code@jacobranson.dev>
2023-02-19 14:08:29 -0500

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,37 @@
+{ pkgs, lib, ... }:
+
+{
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+  nix.settings.trusted-users = [
+    "@admin"
+  ];
+  nix.configureBuildUsers = true;
+
+  nix.extraOptions = ''
+    auto-optimise-store = true
+    experimental-features = nix-command flakes
+  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
+
+  services.nix-daemon.enable = true;
+
+  environment.systemPackages = with pkgs; [];
+
+  fonts.fontDir.enable = true;
+  fonts.fonts = with pkgs; [
+     (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
+   ];
+
+  # Keyboard
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
+
+  security.pam.enableSudoTouchIdAuth = true;
+}
+
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Jacob's darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, darwin, home-manager }: {
+    darwinConfigurations."Jacobs-MacBook-Air" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./configuration.nix
+        home-manager.darwinModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.jacobranson = import ./home.nix;
+          };
+        }
+      ];
+    };
+  };
+}
+
