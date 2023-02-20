5ff4851cafcd559d34db745529c073c80b7dc6b2
Jacob Ranson <code@jacobranson.dev>
2023-02-19 21:24:48 -0500

    Removed workaround

--- home.nix
+++ home.nix
@@ -17,32 +17,10 @@
   # changes in each release.
   home.stateVersion = "22.11";
 
-  home.activation = {
-    # This should be removed once
-    # https://github.com/nix-community/home-manager/issues/1341 is closed.
-    # Taken from https://github.com/NixOS/nix/issues/7055#issuecomment-1250166187
-    aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      app_folder="$(echo ~/Applications)/Home Manager Apps"
-      home_manager_app_folder="$genProfilePath/home-path/Applications"
-      $DRY_RUN_CMD rm -rf "$app_folder"
-      # NB: aliasing ".../home-path/Applications" to "~/Applications/Home Manager Apps" doesn't
-      #     work (presumably because the individual apps are symlinked in that directory, not
-      #     aliased). So this makes "Home Manager Apps" a normal directory and then aliases each
-      #     application into there directly from its location in the nix store.
-      $DRY_RUN_CMD mkdir "$app_folder"
-      for app in $(find "$newGenPath/home-path/Applications" -type l -exec readlink -f {} \;)
-      do
-        $DRY_RUN_CMD osascript \
-          -e "tell app \"Finder\"" \
-          -e "make new alias file at POSIX file \"$app_folder\" to POSIX file \"$app\"" \
-          -e "set name of result to \"$(basename $app)\"" \
-          -e "end tell"
-        done
-    '';
-  };
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # https://github.com/nix-community/home-manager/issues/1341#issuecomment-1301040457
 
   # Terminal emulator
   programs.kitty = {
